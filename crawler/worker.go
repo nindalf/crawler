e6b4c62be01db84ed3cc4bd015c5a433a697f86c
Krishna Sundarram <krishna.sundarram@gmail.com>
2021-04-30 21:48:30 +0100

    Add retryable http client

--- crawler/worker.go
+++ crawler/worker.go
@@ -6,30 +6,30 @@ import (
 	"net/http"
 	"sync"
 	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 type Worker struct {
 	baseUrl string
-	client  *http.Client
+	client  *retryablehttp.Client
 	wg      *sync.WaitGroup
 }
 
 func NewWorker(baseUrl string, wg *sync.WaitGroup) *Worker {
-	client := &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConnsPerHost: 20,
-		},
-		Timeout: 10 * time.Second,
+	client := retryablehttp.NewClient()
+	client.RetryMax = 3
+	client.HTTPClient.Timeout = 10 * time.Second
+	tr := http.Transport{
+		MaxIdleConnsPerHost: 20,
 	}
+	client.HTTPClient.Transport = &tr
 	return &Worker{baseUrl: baseUrl, client: client, wg: wg}
 }
 
 func (w *Worker) Start(urlsToCrawl <-chan string, results chan<- string) {
 	for url := range urlsToCrawl {
-		extractedUrls, err := w.visit(url)
-		if err != nil {
-			log.Fatalln(err)
-		}
+		extractedUrls := w.visit(url)
 		for _, extractedUrl := range extractedUrls {
 			// handle closed channel
 			results <- extractedUrl
@@ -38,12 +38,16 @@ func (w *Worker) Start(urlsToCrawl <-chan string, results chan<- string) {
 	w.wg.Done()
 }
 
-func (w *Worker) visit(url string) ([]string, error) {
+func (w *Worker) visit(url string) []string {
 	fmt.Printf("Visiting url %s\n", url)
 	resp, err := w.client.Get(url)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Non-retriable error while visiting URL - %v\n", err)
 	}
 	defer resp.Body.Close()
-	return Extract(w.baseUrl, resp.Body)
+	urls, err := Extract(w.baseUrl, resp.Body)
+	if err != nil {
+		log.Printf("Failed to parse HTML on url %s. Continuing...", url)
+	}
+	return urls
 }
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/nindalf/crawler
+
+go 1.16
+
+require golang.org/x/net v0.0.0-20210428140749-89ef3d95e781
+require github.com/hashicorp/go-retryablehttp v0.7.0
